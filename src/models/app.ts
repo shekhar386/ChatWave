f29f7eb1d5da2556d9533ce5757bbfc1ee329ef3
Shekhar Paliwal <shekharp3000@gmail.com>
2024-07-01 19:16:11 +0530

    initial commit

---
+++ src/models/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database connection
+mongoose.connect(process.env.MONGOOSE_URI as string).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('MongoDB connection error:', err);
+});
+
+//Routes
+// app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on PORT = ${PORT}'));
+
+export default app;
